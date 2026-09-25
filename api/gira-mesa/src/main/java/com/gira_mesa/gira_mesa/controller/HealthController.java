3e1b4db8bdc6768e4d9f2c239a44ea0a6c622b02
Bruna Lins <brunacalipin@gmail.com>
2026-09-25 20:01:44 -0300

    preparação para deploy

--- api/gira-mesa/src/main/java/com/gira_mesa/gira_mesa/controller/HealthController.java
+++ api/gira-mesa/src/main/java/com/gira_mesa/gira_mesa/controller/HealthController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:5173")
 public class HealthController {
 	
 	private final DataSource dataSource;
